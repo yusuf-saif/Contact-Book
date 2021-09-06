3aad47c34dfc5c6c0f5e2698dc2919ef2512d9de
yusuf-saif <ysaifullah28kbg@gmail.com>
2021-09-06 17:46:20 +0100

    New changes

--- index.php
+++ index.php
@@ -13,61 +13,61 @@
   </thead>
   <tbody>
     <tr class="table-active">
-      <th scope="row">Active</th>
+      <td>Active</td>
       <td>Column content</td>
       <td>Column content</td>
       
     </tr>
     <tr>
-      <th scope="row">Default</th>
+      <td>Default</td>
       <td>Column content</td>
       <td>Column content</td>
     
     </tr>
     <tr class="table-primary">
-      <th scope="row">Primary</th>
+      <td>Primary</td>
       <td>Column content</td>
       <td>Column content</td>
       
     </tr>
     <tr class="table-secondary">
-      <th scope="row">Secondary</th>
+      <td>Secondary</td>
       <td>Column content</td>
       <td>Column content</td>
      
     </tr>
     <tr class="table-success">
-      <th scope="row">Success</th>
+      <td>Success</td>
       <td>Column content</td>
       <td>Column content</td>
       
     </tr>
     <tr class="table-danger">
-      <th scope="row">Danger</th>
+      <td>Danger</td>
       <td>Column content</td>
       <td>Column content</td>
      
     </tr>
     <tr class="table-warning">
-      <th scope="row">Warning</th>
+      <td>Warning</td>
       <td>Column content</td>
       <td>Column content</td>
      
     </tr>
     <tr class="table-info">
-      <th scope="row">Info</th>
+      <td>Info</td>
       <td>Column content</td>
       <td>Column content</td>
      >
     </tr>
     <tr class="table-light">
-      <th scope="row">Light</th>
+      <td>Light</td>
       <td>Column content</td>
       <td>Column content</td>
       
     </tr>
     <tr class="table-dark">
-      <th scope="row">Dark</th>
+      <td>Dark</td>
       <td>Column content</td>
       <td>Column content</td>
      

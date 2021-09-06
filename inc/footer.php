24ef829fd8c0f6ace499579bdcda9fa1f3448805
yusuf-saif <ysaifullah28kbg@gmail.com>
2021-09-06 17:36:03 +0100

    New files

---
+++ inc/footer.php
@@ -0,0 +1,9 @@
+ 
+ <div
+         class="text-center p-3"
+         style="background-color: rgba(0, 0, 0, 0.2)"
+         >
+      © 2021 Copyright:
+      <a class="text-white" href="#">Contact-Book App</a>
+    </div>
+  
---
+++ inc/header.php
@@ -0,0 +1,48 @@
+<html lang="en">
+<head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+      <title>Contact App</title>
+</head>
+<body>
+  <!-- Header Start-->
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">My-Contact-Book</a>
+    <div class="collapse navbar-collapse fs-3 d-flex justify-content-evenly" id="navbarColor03">
+      <ul class="navbar-nav me-auto">
+        <li class="nav-item">
+          <a class="nav-link active" href="#">Contacts
+            <span class="visually-hidden">(current)</span>
+          </a>
+        </li>
+      <li class="nav-item">
+          <a class="nav-link" href="#">Create </a>
+     </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Update </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Delete   </a>
+        </li>
+        </ul>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor03" aria-controls="navbarColor03" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>  
+    <div class="d-flex justify-content-center">
+    <form class="d-flex justify-content-center">
+        <input class="form-control me-sm-2 " type="text" placeholder="Search">
+        <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
+      </form>
+    </div>
+</nav>
+      <!-- Header Ends -->
+
+
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+</body>
+</html>

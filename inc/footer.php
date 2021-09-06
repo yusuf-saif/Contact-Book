783a45db0a38eac13d5c4c2f9cf6e7c9e18cbaff
yusuf-saif <ysaifullah28kbg@gmail.com>
2021-09-06 17:38:15 +0100

    new files

--- inc/footer.php
+++ inc/footer.php
@@ -1,9 +1,6 @@
  
- <div
-         class="text-center p-3"
-         style="background-color: rgba(0, 0, 0, 0.2)"
-         >
-      © 2021 Copyright:
+ <div class="text-center p-3 bg-light >
+      ©& 2021 Copyright:
       <a class="text-white" href="#">Contact-Book App</a>
     </div>
   

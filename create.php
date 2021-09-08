7f5076b59956f9ebdca5f1592ce8bc9a21a5e9de
yusuf-saif <ysaifullah28kbg@gmail.com>
2021-09-08 09:39:43 +0100

    Create page

---
+++ create.php
@@ -0,0 +1,27 @@
+<?php 
+      include 'inc/header.php'
+?>
+
+<form class="row g-3">
+  <div class="col-md-6">
+    <label for="fullname" class="form-label">FullName</label>
+    <input type="text" class="form-control" id="fullname">
+  </div> 
+  <div class="col-md-6">
+    <label for="phone" class="form-label">Phone Number</label>
+    <input type="number" class="form-control" id="phone">
+  </div>
+  <div class="col-md-6">
+    <label for="inputEmail4" class="form-label">Email</label>
+    <input type="email" class="form-control" id="inputEmail4">
+  </div>
+ 
+</form>
+
+
+
+
+<?php 
+      include 'inc/footer.php'
+?>
+
